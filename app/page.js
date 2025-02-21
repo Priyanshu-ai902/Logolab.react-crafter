7ba54371c4100cdfe8ac4c97f867bbfeb0d21daf
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2025-02-21 16:46:38 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>helo</h1>
+      <Button>HELLO</Button>
+    </div>
+  );
+}
