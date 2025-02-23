0b8e0eb17db58fc49fc32d30f7757030cd2c3f18
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2025-02-23 22:36:59 +0530

    auth added

--- app/create/_components/PricingModel.jsx
+++ app/create/_components/PricingModel.jsx
@@ -1,9 +1,15 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { SignInButton, useUser, useClerk } from "@clerk/nextjs";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function PricingModel({ fromData }) {
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+  const router = useRouter();
 
   useEffect(() => {
     if (fromData && Object.keys(fromData).length > 0 && typeof window !== "undefined") {
@@ -11,19 +17,25 @@ function PricingModel({ fromData }) {
       localStorage.setItem("fromData", JSON.stringify(fromData));
     }
   }, [fromData]);
-  
-
 
   console.log("Data received in PricingModel:", fromData);
 
-
+  const handleGenerateClick = async () => {
+    if (user) {
+      router.push("/generate-logo");
+    } else {
+      const signIn = await openSignIn();
+      if (signIn) {
+        router.push("/generate-logo");
+      }
+    }
+  };
 
   return (
-
-    <div className="flex justify-center items-center ">
+    <div className="flex justify-center items-center">
       <div className="bg-white rounded-2xl shadow-lg p-6 w-80 text-center">
         <div className="mb-4">
-          <span className="inline-block bg-red-500 text-white text-sm font-bold px-3 py-1  rounded-full">
+          <span className="inline-block bg-red-500 text-white text-sm font-bold px-3 py-1 rounded-full">
             Your Logo is ready
           </span>
         </div>
@@ -34,13 +46,15 @@ function PricingModel({ fromData }) {
           <li>• Wait time: 30 seconds to 3 minutes</li>
           <li>• Limited Design Options and Quality</li>
         </ul>
-        <Button className="mt-6 w-full bg-pink-600 hover:bg-pink-700 text-white">
+
+        <Button
+          className="mt-6 w-full bg-pink-600 hover:bg-pink-700 text-white"
+          onClick={handleGenerateClick}
+        >
           Generate
         </Button>
       </div>
     </div>
-
-
   );
 }
 
---
+++ app/generate-logo/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GenerateLogo() {
+  return (
+    <div className='bg-black text-white'>
+      generate logo
+    </div>
+  )
+}
+
+export default GenerateLogo
